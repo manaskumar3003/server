1b90bef14b37c4b301665798f932ffe2375982a1
Manas Kumar <141910018+manaskumar3003@users.noreply.github.com>
2024-07-21 01:17:16 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
 
 
-const auth = (req,res,next)=>{
-    try{
-        const token = req.header("Authorization")
-        if(!token) return res.status(400).json({msg:"invalid auth"})
-            jwt.verify(token,"SECRETTOKEN",(err,user)=>{
-        if(err) return res.status(400).json({msg:"invalid authenication"})
+// const auth = (req,res,next)=>{
+//     try{
+//         const token = req.header("Authorization")
+//         if(!token) return res.status(400).json({msg:"invalid auth"})
+//             jwt.verify(token,"SECRETTOKEN",(err,user)=>{
+//         if(err) return res.status(400).json({msg:"invalid authenication"})
 
-        req.user = user
-        next()
-            })
+//         req.user = user
+//         next()
+//             })
 
-    }catch(err){
-        return res.status(500).json({msg:err.message})
+//     }catch(err){
+//         return res.status(500).json({msg:err.message})
 
-    }
-}
+//     }
+// }
 
-module.exports = auth
+// module.exports = auth
